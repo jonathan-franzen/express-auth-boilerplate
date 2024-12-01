3d148d49c4d254caea16a272254197b1df610a8d
Jonathan Franzen <jonathan.franzen@gigger.se>
2024-12-01 17:09:49 +0100

    Remove unsude sleep functions

--- src/utils/sleep.ts
+++
@@ -1,9 +0,0 @@
-export function sleep(ms: number): Promise<void> {
-	return new Promise<void>(
-		(resolve: () => void): NodeJS.Timeout => setTimeout(resolve, ms),
-	);
-}
-
-export function exponentialSleep(tryCount: number): Promise<void> {
-	return sleep(1000 * Math.pow(2, tryCount));
-}
