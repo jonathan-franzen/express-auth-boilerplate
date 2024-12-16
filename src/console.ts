2230e92bc28ada0c30d182810f667ee3da0a6bd4
Jonathan Franzen <jonathan.franzen@gigger.se>
2024-12-17 00:28:03 +0100

    Use console package

--- src/console.ts
+++ src/console.ts
@@ -1,32 +1,3 @@
-import { exec } from 'child_process';
-import { until } from '@open-draft/until';
+import { serverlessConsole } from 'serverless-console';
 
-export const handler = async (command: string): Promise<void> => {
-	const { data, error } = await until(
-		() =>
-			new Promise<string>((resolve, reject) => {
-				exec(command, (error, stdout, stderr) => {
-					if (error) {
-						reject(stderr || error.message);
-					} else {
-						resolve(stdout);
-					}
-				});
-			}),
-	);
-
-	if (error) {
-		error.message.split('\n').forEach((line) => {
-			if (line.trim()) {
-				console.error(line);
-			}
-		});
-		return;
-	}
-
-	data.split('\n').forEach((line) => {
-		if (line.trim()) {
-			console.log(line);
-		}
-	});
-};
+export const handler: (command: string) => Promise<void> = serverlessConsole;
